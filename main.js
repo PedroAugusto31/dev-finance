f12bcff0e5e0f2ae05f7da913c997bda423ae7fa
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-09-09 16:42:58 -0300

    format ready

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
-const entrys = document.querySelector(".entrys h2");
-const exits = document.querySelector(".exits h2");
-const total = document.querySelector(".total h2");
+const entryElement = document.querySelector(".entrys h2");
+const exitElement = document.querySelector(".exits h2");
+const totalElement = document.querySelector(".total h2");
+
+let entryTotal = entryElement.innerHTML.split(" ");
+let entryMoney = entryTotal[1].replace(".", "").replace(",00", "");
+
+let exitTotal = exitElement.innerHTML.split(" ");
+let exitMoney = exitTotal[1].replace(".", "").replace(",00", "");
+
+function moneyUpdate(money) {
+	let brMoney = new Intl.NumberFormat("pt-BR", {
+		style: "currency",
+		currency: "BRL",
+	}).format(money);
+
+	return brMoney;
+}
+
+let totalMoney = moneyUpdate(parseInt(entryMoney) - parseInt(exitMoney));
+totalElement.textContent = totalMoney;
