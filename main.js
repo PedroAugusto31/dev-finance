ee0d4189853ca4d11c18a75b7f0da5210876270e
PedroAugusto31 <76188013+PedroAugusto31@users.noreply.github.com>
2021-09-10 08:49:42 -0300

    adding transitions

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ let entryMoney = entryTotal[1].replace(".", "").replace(",00", "");
 let exitTotal = exitElement.innerHTML.split(" ");
 let exitMoney = exitTotal[1].replace(".", "").replace(",00", "");
 
+let totalMoney = moneyUpdate(parseInt(entryMoney) - parseInt(exitMoney));
+totalElement.textContent = totalMoney;
+
 function moneyUpdate(money) {
 	let brMoney = new Intl.NumberFormat("pt-BR", {
 		style: "currency",
@@ -16,6 +19,3 @@ function moneyUpdate(money) {
 
 	return brMoney;
 }
-
-let totalMoney = moneyUpdate(parseInt(entryMoney) - parseInt(exitMoney));
-totalElement.textContent = totalMoney;
--- modal.js
+++ modal.js
@@ -1,11 +1,23 @@
 const modalButton = document.querySelector("button.modal-button");
 const cancelButton = document.querySelector("button.cancel-button");
+const sendButton = document.querySelector("button.save-button");
 const modalWrapper = document.querySelector("div.modal-wrapper");
 const modal = document.querySelector("div.modal");
 
 modalButton.addEventListener("click", open);
 
 cancelButton.addEventListener("click", close);
+
+sendButton.addEventListener("click", (e) => {
+	e.preventDefault();
+
+	const descrition = document.querySelector("#descrition");
+	console.log(descrition.value);
+	const value = document.querySelector("#value");
+	console.log(moneyUpdate(value.value));
+	const date = document.querySelector("#date");
+	console.log(date.value);
+});
 
 function open() {
 	modalWrapper.classList.add("active");
